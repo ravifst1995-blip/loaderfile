a9f35487c561cd5c99a5c74addfb193e06f31670
rajverma098 <raj.w3web@gmail.com>
2025-08-25 00:24:48 +0530

    someissuform

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import DriverHero from "./components/homepage/DriverHero";
-import DriverFinderForm from "./components/homepage/DriverFinderForm";
+
 import SupportTiles from "./components/homepage/SupportTiles";
 import PrinterSetupSection from "./components/homepage/PrinterSetupSection";
 import SupportCopySection from "./components/homepage/SupportCopySection";
